c7d40e65ad69d9337e6b472692aa646e5d77f7bb
AhmedAbd-Allah <ahmedabdallah.programming@gmail.com>
2018-03-30 16:45:21 +0200

    rating model

---
+++ Nodejs/models/rating.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ratings = new Schema({
+    product_id: 
+    {
+        type: Schema.Types.ObjectId,
+        ref:"products"
+    },
+    user_id: 
+    {
+        type: Schema.Types.ObjectId,
+        ref:"users"
+    },
+    rating: 
+    {
+        type:Number
+    }
+
+});
+mongoose.model("ratings",ratings);
