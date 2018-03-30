1dd4dd5fc9bdc1e7900ae830aa375beb813f5c8a
aliaa@aliaa-satellite-c850-a826 <aliaasayed45@gmail.com>
2018-03-31 00:17:14 +0200

    show all sub categoris in filter

--- Angular/souq/src/app/souq-search/souq-search.component.ts
+++ Angular/souq/src/app/souq-search/souq-search.component.ts
@@ -17,11 +17,11 @@ export class SouqSearchComponent implements OnInit {
   minVal=0;
   maxVal=100000;
   priceRange=this.minVal+":"+0
-  options = [
-    {name:'Bags & Wallets', value:'1', checked:false},
-    {name:'shoes', value:'2', checked:false},
-    {name:'OptionC', value:'3', checked:false}
-  ];
+  options = []
+  //   {name:'Bags & Wallets', value:'1', checked:false},
+  //   {name:'shoes', value:'2', checked:false},
+  //   {name:'OptionC', value:'3', checked:false}
+  // ];
   categories;
 
 
@@ -29,11 +29,26 @@ export class SouqSearchComponent implements OnInit {
 
     this.categoriesService.getCategories().subscribe((res)=>{
       this.categories=res;
+      for(var i=0;i<this.categories.length;i++)
+      {
+        for(var j=0;j<this.categories[i].subcategories.length;j++)
+        {
+          var option={name:'',value:0,checked:false};
+
+          console.log(this.categories[i].subcategories[j]);
+
+        option.name=this.categories[i].subcategories[j]
+        option.value=j+1
+        option.checked=false
+        this.options.push(option);
+      }
+      }
       console.log(this.categories.length);
-     
+      console.log(this.options);
+
     });
-   
-  
+
+
 
     // for(var i=0;i<this.categories.length;i++)
     // {
@@ -41,13 +56,13 @@ export class SouqSearchComponent implements OnInit {
     //   var subcategory=this.categories[i].subcategories;
     //   this.options.push({name:subcategory,value:subcategory,checked:false});
     // }
-    // console.log(this.options) 
-   
+    // console.log(this.options)
+
    }
 
 
   ngOnInit() {
-    console.log(this.categories) 
+    console.log(this.categories)
   }
 //check boxes
 
